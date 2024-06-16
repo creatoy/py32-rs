6363f16918c74e81ccae3ae6eead263a0de7a095
creatoy <yang@creatoy.top>
2024-06-16 14:48:36 +0800

    Update svd files

--- svd/extract.sh
+++ svd/extract.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Copy svd files and rename to remove trailing 'x'
-cp vendor/py32f002axx.svd py32f002a.svd
-cp vendor/py32f003xx.svd py32f003.svd
-cp vendor/py32f030xx.svd py32f030.svd
+cp vendor/PY32F002Axx.svd py32f002a.svd
+cp vendor/PY32F002Bxx.svd py32f002b.svd
+cp vendor/PY32F003xx.svd py32f003.svd
+cp vendor/PY32F030xx.svd py32f030.svd
